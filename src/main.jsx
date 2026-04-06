6d842be88130fb0e1421cae2469fafca92afeff4
Nguyễn Minh Duy <217141310+duyxyz@users.noreply.github.com>
2026-04-07 05:23:27 +0700

    feat: Triển khai PWA Cache giúp tăng tốc độ tải trang và hỗ trợ hoạt động ngoại tuyến (offline)._

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { registerSW } from 'virtual:pwa-register'
+
+// Tự động đăng ký và cập nhật service worker
+registerSW({ immediate: true })
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
+
